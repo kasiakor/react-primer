65d4fb87752f65af74355796cf3a674e13126c3e
kasia.kordys <kasia.kordys@gmail.com>
2023-03-21 20:54:32 +0200

    use function in an expression

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
 
+const number = 5;
+
+function isEven() {
+  return number % 2 === 0 ? "even" : "odd";
+}
+
 export default class App extends Component {
   render() {
     return (
       <div>
         <h4 className="bg-primary text-white text-center p-2 m-1">
-          Component Element
+          Number of things: {isEven()}
         </h4>
       </div>
     );
