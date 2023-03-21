f65a5e3e7588b77ab4e5f4454592aba8259aae2e
kasia.kordys <kasia.kordys@gmail.com>
2023-03-21 21:12:10 +0200

    use this keyword to specify properties and methods in the component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React, { Component } from "react";
 
-const number = 5;
-
-function isEven() {
-  return number % 2 === 0 ? "even" : "odd";
-}
-
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      number: 8,
+    };
+  }
+
+  isEven() {
+    return this.state.number % 2 === 0 ? "even" : "odd";
+  }
+
   render() {
     return (
       <div>
         <h4 className="bg-primary text-white text-center p-2 m-1">
-          Number of things: {isEven()}
+          Number of things: {this.isEven()}
         </h4>
       </div>
     );
