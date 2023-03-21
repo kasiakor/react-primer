61c1b3e965d91c0b1bb93114c4e92d921bc4a5da
kasia.kordys <kasia.kordys@gmail.com>
2023-03-21 21:26:28 +0200

    set prop values

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      number: 8,
+      number: 1,
     };
   }
 
@@ -12,10 +12,16 @@ export default class App extends Component {
     return this.state.number % 2 === 0 ? "even" : "odd";
   }
 
+  getClassName(val) {
+    return val % 2 === 0
+      ? "bg-primary text-white text-center p-2 m-1"
+      : "bg-secondary text-white text-center p-2 m-1";
+  }
+
   render() {
     return (
       <div>
-        <h4 className="bg-primary text-white text-center p-2 m-1">
+        <h4 className={this.getClassName(this.state.number)}>
           Number of things: {this.isEven()}
         </h4>
       </div>
